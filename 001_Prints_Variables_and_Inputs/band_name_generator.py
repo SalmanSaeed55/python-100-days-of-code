c4d12c7aea298d47a307e5a74474b0aeffd13100
SalmanSaeed55 <salmansaeed1359@gmail.com>
2026-06-03 12:28:25 +0100

    Day 1/100: Prints, Concatination and Inputs complete. Day 1 project: Simple band name generator taking in city and pet names and combining them

---
+++ 001_Prints_Variables_and_Inputs/band_name_generator.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator")
+city = input("Enter the city you grew up in\n\t")
+pet_name = input("Enter your pet's name\n\t")
+
+# print("Your band name could be " + city + " " + pet_name)
+print(f"Your band name could be {city} {pet_name}")
---
+++ 001_Prints_Variables_and_Inputs/day_001_tutorial/tutorial.py
@@ -0,0 +1,31 @@
+# Printing
+print("Hello World")
+print("Hello World")
+print("Hello World")
+
+print("")
+
+# New Lines without multiple prints
+print("Hello World\nHello World\nHello World")
+
+print()
+
+# Concatenation
+print("Hello" + "Salman")  # No space in between
+print("Hello" + " " + "Salman")
+
+print()
+
+# Input function
+# input("Enter your name: ")
+
+print("Hello " + input("Enter your name: ") + "!") # input asked first, and then prints the message
+
+# Variables
+name = input("Enter your name: ")
+print(name)
+
+# Calculating length of a string
+user_name = input("Enter your name: ")
+length_user = len(user_name)
+print(length_user)
