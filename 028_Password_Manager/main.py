54b7cc9ede833b5b56814136d4d5eb0050f47ec0
SalmanSaeed55 <salmansaeed1359@gmail.com>
2026-07-17 20:04:24 +0100

    Add Password Manager UI: implement labels for website, email, and password fields

--- 028_Password_Manager/main.py
+++ 028_Password_Manager/main.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from tkinter import *
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -10,9 +11,19 @@ window.title("Salman's Password Manager")
 window.config(padx=30, pady=30)
 
 canvas = Canvas(width=200, height=200)
-logo = PhotoImage(file="./logo.png")
+logo_path = Path(__file__).resolve().parent / "logo.png"
+logo = PhotoImage(file=str(logo_path))
 canvas.create_image(100, 100, image=logo)
 canvas.grid(row=0, column=1)
-canvas.pack()
+
+website_label = Label(text="Website:")
+website_label.grid(row=1, column=0)
+
+email_label = Label(text="Email Address:")
+email_label.grid(row=2, column=0)
+
+password_label = Label(text="Password:")
+password_label.grid(row=3, column=0)
+
 
 window.mainloop()
