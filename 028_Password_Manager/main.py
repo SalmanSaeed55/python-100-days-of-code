dad019218f72bab95d5df3c805b49d1b758df171
SalmanSaeed55 <salmansaeed1359@gmail.com>
2026-07-18 19:24:06 +0100

    Add Password Manager UI: enhance layout with entry fields for website, email, and password; include 'Add Password' button

--- 028_Password_Manager/main.py
+++ 028_Password_Manager/main.py
@@ -17,13 +17,22 @@ canvas.create_image(100, 100, image=logo)
 canvas.grid(row=0, column=1)
 
 website_label = Label(text="Website:")
-website_label.grid(row=1, column=0)
+website_label.grid(row=1, column=0, sticky="w")
+website_entry = Entry(width=40)
+website_entry.grid(row=1, column=1, columnspan=2)
 
 email_label = Label(text="Email Address:")
-email_label.grid(row=2, column=0)
+email_label.grid(row=2, column=0, sticky="w")
+email_entry = Entry(width=40)
+email_entry.grid(row=2, column=1, columnspan=2)
 
 password_label = Label(text="Password:")
-password_label.grid(row=3, column=0)
+password_label.grid(row=3, column=0, sticky="w")
+password_entry = Entry(width=30)
+password_entry.grid(row=3, column=1, sticky="w")
+
+add_password = Button(text="Add Password", width=50)
+add_password.grid(row=4, column=0, columnspan=3)
 
 
 window.mainloop()
