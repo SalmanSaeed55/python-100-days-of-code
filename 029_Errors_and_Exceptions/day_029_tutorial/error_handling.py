5b8e8846806e5a3bc02a1209157bae825eff342f
SalmanSaeed55 <salmansaeed1359@gmail.com>
2026-07-22 19:54:11 +0100

    Add error handling for user input: implement try-except for ValueError in number input

--- 029_Errors_and_Exceptions/day_029_tutorial/error_handling.py
+++ 029_Errors_and_Exceptions/day_029_tutorial/error_handling.py
@@ -10,3 +10,11 @@ try:
     print(new_list[5])
 except IndexError:
     print("Index Error")
+
+
+try:
+    num = int(input("Enter a number: "))
+except ValueError:
+    print("Invalid input")
+else:
+    print(type(num))
