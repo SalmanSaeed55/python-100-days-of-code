eb41a67311e46e232b5e9a2acb6d456436544b9a
SalmanSaeed55 <salmansaeed1359@gmail.com>
2026-09-23 14:31:25 +0100

    Add web scraping functionality: extract links, addresses, and prices from Zillow listings

--- 048_Data_Entry_Automation/main.py
+++ 048_Data_Entry_Automation/main.py
@@ -1,5 +1,35 @@
 from bs4 import BeautifulSoup
 import requests
-import selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
 
 FORM_URL = "https://forms.gle/gR5aFTxJyg5hwuQH9"
+HOUSES_URL = "https://appbrewery.github.io/Zillow-Clone/"
+
+header = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36",
+    "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
+}
+
+response = requests.get(HOUSES_URL, headers=header)
+
+data = response.text
+soup = BeautifulSoup(data, "html.parser")
+
+all_link_elements = soup.select(".StyledPropertyCardDataWrapper a")
+
+all_links = [link["href"] for link in all_link_elements]
+print(f"There are {len(all_links)} links to individual listings in total: \n")
+print(all_links)
+
+all_address_elements = soup.select(".StyledPropertyCardDataWrapper address")
+all_addresses = [address.get_text().replace(" | ", " ").strip() for address in all_address_elements]
+print(f"\n After having been cleaned up, the {len(all_addresses)} addresses now look like this: \n")
+print(all_addresses)
+
+all_price_elements = soup.select(".PropertyCardWrapper span")
+all_prices = [price.get_text().replace("/mo", "").split("+")[0] for price in all_price_elements if "$" in price.text]
+print(f"\n After having been cleaned up, the {len(all_prices)} prices now look like this: \n")
+print(all_prices)
+
